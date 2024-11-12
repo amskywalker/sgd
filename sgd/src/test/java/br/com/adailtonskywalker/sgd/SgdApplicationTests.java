86a229d42e3b6ed95cbefeed6f1168b2ae2a96d7
Adailton Moura <contatoadailtonmoura@gmail.com>
2024-11-11 23:19:18 -0300

    first commit

---
+++ sgd/src/test/java/br/com/adailtonskywalker/sgd/SgdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adailtonskywalker.sgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
