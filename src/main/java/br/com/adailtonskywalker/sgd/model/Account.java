6408e41dc0b6e501eb6ad58563ee48034a6f48ff
Adailton Moura <contatoadailtonmoura@gmail.com>
2024-11-12 23:27:45 -0300

    feature: add user and transaction relationships in account table

--- src/main/java/br/com/adailtonskywalker/sgd/model/Account.java
+++ src/main/java/br/com/adailtonskywalker/sgd/model/Account.java
@@ -3,6 +3,8 @@ package br.com.adailtonskywalker.sgd.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -12,4 +14,18 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+
+    private Float balance = 0.0f;
+
+    private Boolean active = true;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> transactions;
 }
