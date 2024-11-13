741b05723ab95c800ca2f8a30e6cd4d23e7192e6
Adailton Moura <contatoadailtonmoura@gmail.com>
2024-11-12 23:26:35 -0300

    feature: add installment plan relationship in installment table

--- src/main/java/br/com/adailtonskywalker/sgd/model/Installment.java
+++ src/main/java/br/com/adailtonskywalker/sgd/model/Installment.java
@@ -3,6 +3,7 @@ package br.com.adailtonskywalker.sgd.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -12,4 +13,20 @@ public class Installment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false)
+    private Integer number;
+
+    @Column(nullable = false)
+    private Float amount;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "installment_plan_id", nullable = false)
+    private InstallmentPlan installmentPlan;
 }
