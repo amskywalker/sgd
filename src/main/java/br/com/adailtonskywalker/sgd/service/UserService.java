4ead3ceb37457c06b56ec5a6353f178124f5fa25
Adailton Moura <contatoadailtonmoura@gmail.com>
2024-11-17 23:20:19 -0300

    feature: update user service to encode password

--- src/main/java/br/com/adailtonskywalker/sgd/service/UserService.java
+++ src/main/java/br/com/adailtonskywalker/sgd/service/UserService.java
@@ -5,6 +5,7 @@ import br.com.adailtonskywalker.sgd.dto.UserRequestData;
 import br.com.adailtonskywalker.sgd.model.User;
 import br.com.adailtonskywalker.sgd.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,20 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
     private final UserRepository userRepository;
     private final AccountService accountService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, AccountService accountService) {
+    public UserService(UserRepository userRepository, AccountService accountService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.accountService = accountService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public User save(UserRequestData userRequestData) {
         User user = new User();
         user.setName(userRequestData.getName());
-        user.setPassword(userRequestData.getPassword());
+        user.setPassword(passwordEncoder.encode(userRequestData.getPassword()));
         user.setUsername(userRequestData.getUsername());
+        User createdUser = userRepository.save(user);
         accountService.save(AccountRequestData.builder().user(user).build());
-        return userRepository.save(user);
+        return createdUser;
     }
 }
