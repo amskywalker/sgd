86164f76cb84dc600c9fc90a689b1dadd8cc1798
Adailton Moura <contatoadailtonmoura@gmail.com>
2024-11-23 00:48:43 -0300

    feature: create input/output transaction dto

--- src/main/java/br/com/adailtonskywalker/sgd/dto/TransactionResponseData.java
+++ src/main/java/br/com/adailtonskywalker/sgd/dto/TransactionResponseData.java
@@ -1,25 +1,19 @@
 package br.com.adailtonskywalker.sgd.dto;
 
 import br.com.adailtonskywalker.sgd.model.TransactionType;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @Builder
-public class TransactionRequestData {
-    @NotNull
+public class TransactionResponseData {
+    private UUID id;
     private TransactionType type;
-    @NotNull
-    @Size(min = 1, max = 100)
     private String description;
-    @NotNull
     private LocalDateTime date;
-    @NotNull
     private Float amount;
-    @NotNull
-    private String accountUuid;
+    private AccountResponseData account;
 }
