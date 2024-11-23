c9ac4c18aad953f35d55c217f19726b3f6e3bbe4
Adailton Moura <contatoadailtonmoura@gmail.com>
2024-11-23 00:51:12 -0300

    feature: implements create transaction from account

---
+++ src/main/java/br/com/adailtonskywalker/sgd/controller/TransactionController.java
@@ -0,0 +1,26 @@
+package br.com.adailtonskywalker.sgd.controller;
+
+import br.com.adailtonskywalker.sgd.dto.TransactionRequestData;
+import br.com.adailtonskywalker.sgd.dto.TransactionResponseData;
+import br.com.adailtonskywalker.sgd.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+    private final TransactionService service;
+
+    @Autowired
+    public TransactionController(TransactionService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/")
+    public TransactionResponseData save(@Valid @RequestBody TransactionRequestData data) {
+        return service.save(data);
+    }
+}
